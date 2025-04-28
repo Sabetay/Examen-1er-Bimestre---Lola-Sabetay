b4ae0a1b36e9a4252be039ddf84775a3ddee93ff
Sabetay <lolasabetay@gmail.com>
2025-04-28 11:59:19 -0300

    me falta el debg final y el %

---
+++ Assets/FilaVirtual.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FilaVirtual : MonoBehaviour
+{
+    public string CodigoEntrada;
+    public int CantEntradas;
+    public string Prioridad;
+    float precioTotal;
+    float precioBasico = 800;
+    float precioNormal = 1200;
+    float precioPremium = 2000;
+    float Descuento;
+
+    void Start()
+    {
+        Debug.Log("Ingrese Codigo Entrada");
+        if (CodigoEntrada != "b" | CodigoEntrada != "p" | CodigoEntrada != "n")
+        {
+
+            Debug.Log("Codigo entrada no valido");
+            return;
+        }
+        Debug.Log("Ingrese Cantidad de Entradas");
+
+        if (CantEntradas < 4 & CodigoEntrada == "b")
+        {
+            precioTotal = CantEntradas * precioBasico;
+        }
+        else
+        {
+            Debug.Log("Cantidad Entradas no valido");
+            return;
+        }
+        if (CantEntradas < 6 & CodigoEntrada == "n")
+        {
+            precioTotal = CantEntradas * precioNormal;
+        }
+        else
+        {
+            Debug.Log("Cantidad Entradas no valido");
+            return;
+        }
+        Debug.Log("DeseaFilaPremium");
+        if (Prioridad == "si")
+        {
+            if (CantEntradas > 0 & CodigoEntrada == "p")
+            {
+                precioTotal = (CantEntradas * precioPremium) + 15; // me tira error el signo de porcentaje
+            }
+            else
+            {
+                Debug.Log("Cantidad Entradas no valido");
+                return;
+            }
+        }
+        else if (Prioridad == "no")
+        {
+            if (CantEntradas > 0 & CodigoEntrada == "p")
+            {
+                precioTotal = (CantEntradas * 2000);
+            }
+            else
+            {
+                Debug.Log("Cantidad Entradas no valido");
+                return;
+            }
+
+        }
+
+    }
+}
+
+
